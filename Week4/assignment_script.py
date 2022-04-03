93f737ca8ab83cf951171b644aec7b213326b414
ManOfAran <101205549+TheBigRJM@users.noreply.github.com>
2022-04-03 17:57:50 +0100

    Edits - Exercise script Week 3

--- Week4/assignment_script.py
+++ Week4/assignment_script.py
@@ -64,8 +64,42 @@ def img_display(img, ax, bands, stretch_args=None, **imshow_args):
 # note - rasterio's open() function works in much the same way as python's - once we open a file,
 # we have to make sure to close it. One easy way to do this in a script is by using the with statement shown
 # below - once we get to the end of this statement, the file is closed.
-with rio.open('data_files/NI_Mosaic.tif') as dataset:
+data_folder = 'D://UlsterProgramming//egm722//Week4//data_files//'
+with rio.open(data_folder + '/NI_Mosaic.tif') as dataset:
     img = dataset.read()
     xmin, ymin, xmax, ymax = dataset.bounds
 
 # your code goes here!
+
+
+
+outline = gpd.read_file('Week2/data_files/NI_outline.shp')
+towns = gpd.read_file('Week2/data_files/Towns.shp')
+water = gpd.read_file('Week2/data_files/Water.shp')
+rivers = gpd.read_file('Week2/data_files/Rivers.shp')
+counties = gpd.read_file('Week2/data_files/Counties.shp')
+
+
+# create a figure of size 10x10 (representing the page size in inches)
+myFig = plt.figure(figsize=(10, 10))
+
+myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
+
+ax = plt.axes(projection=ccrs.Mercator())  # finally, create an axes object in the figure, using a Mercator
+# projection, where we can actually plot our data.
+
+# first, we just add the outline of Northern Ireland using cartopy's ShapelyFeature
+outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='w')
+
+xmin, ymin, xmax, ymax = outline.total_bounds
+ax.add_feature(outline_feature) # add the features we've created to the map.
+
+# using the boundary of the shapefile features, zoom the map to our area of interest
+ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS) # because total_bounds gives output as xmin, ymin, xmax, ymax,
+# but set_extent takes xmin, xmax, ymin, ymax, we re-order the coordinates here.
+
+# pick colors, add features to the map
+county_colors = ['firebrick', 'seagreen', 'royalblue', 'coral', 'violet', 'cornsilk']
+
+
+img_display(dataset,ax, 1,2,3)
